0df24513a47a01b2c1eb6bad8b649d9b8e5eecd2
Dylan Grafmyre <thorsummoner@live.com>
2016-08-01 19:08:19 -0700

    document this file better

--- linkedpanes.py
+++ linkedpanes.py
@@ -1,7 +1,18 @@
+""" Gtk Widget for linked 2x2 panes
+"""
 
 from gi.repository import Gtk
 
 class LinkedPanes(Gtk.Bin):
+    """ Linked Panes is a set of three paned widgets, one top level one
+        and two nested ones that keep in sync with eachother.
+
+        Args:
+            tl (Gtk.Widget): Top left widget
+            tr (Gtk.Widget): Top right widget
+            bl (Gtk.Widget): Bottom left widget
+            br (Gtk.Widget): Bottom right widget
+    """
     def __init__(self, tl, tr, bl, br):
         super(LinkedPanes, self).__init__()
 
@@ -30,8 +41,15 @@ class LinkedPanes(Gtk.Bin):
             )
 
 class Pane(Gtk.Paned):
-    """docstring for Paned"""
+    """ Gtk.Paned wrapper for easier building.
 
+        Args:
+            orientation (Gtk.Orientation): Horizontal or vertical orientation.
+            child1 (Gtk.Widget): First widget to pack
+            child2 (Gtk.Widget): Second widget to pack
+            size (tuple): Pixel width-and-height-packed tuple.
+            shadow (Gtk.ShadowType, optional): Shadow type, assumed "in" for best visual ease.
+    """
 
     def __init__(self, orientation, child1, child2, size, shadow=Gtk.ShadowType.IN):
         super(Pane, self).__init__(orientation=orientation)
@@ -49,6 +67,11 @@ class Pane(Gtk.Paned):
         self.frame2.add(child2)
 
 class LinkedPane(Pane):
+    """ Pane wrapper with methods for position synchronizing.
+
+        Args:
+            *args (tuple): Passed to :py:class:`Paned`
+    """
 
     _lp_user_activated = False
 
@@ -59,13 +82,25 @@ class LinkedPane(Pane):
         self.connect('button-release-event', self.on_button_release)
 
     def on_position(self, _, gparamspec):
+        """ `notify::position` handler
+            Updates sibling pane with new position.
+        """
         if self._lp_user_activated:
             self.linked.child_on_position(self.props.position)
 
     def child_on_position(self, position):
+        """ Sibling `notify::position` handler.
+            This responds to sibling position updates.
+        """
         self.set_position(position)
 
     def on_button_press(self, *_):
+        """ Record when event happened directly to this widget (by a user
+            mouse input) vs when it happens on another widget (eg the
+            window is resized).
+            We only wish to resize on user events as the window resize
+            cascades its own resize events correctly already.
+        """
         self._lp_user_activated = True
 
     def on_button_release(self, *_):
@@ -77,4 +112,6 @@ class LinkedPane(Pane):
         self.linked.set_position(self.props.position)
 
     def bind_resize(self, linked):
+        """ Realize the sibling link.
+        """
         self.linked = linked
