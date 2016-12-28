7a6d0d6e936cc2726a0fc9adb1eb7b562a2bda44
Dylan Grafmyre <thorsummoner@live.com>
2016-12-27 19:09:07 -0800

    Package up

--- gtk_panedquadrent.py
+++ gtk_panedquadrent.py
@@ -1,9 +1,29 @@
-""" Gtk Widget for linked 2x2 panes
+# pylint: disable=too-many-arguments
+#
+# Gtk.PanedQuadrant Synchronized Nested Gtk.Paned Widgets
+# Copyright (C) 2014  Dylan Scott Grafmyre <thorsummoner@live.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+    Gtk.PanedQuadrant Synchronized Nested Gtk.Paned Widgets
 """
 
 from gi.repository import Gtk
 
-class LinkedPanes(Gtk.Bin):
+
+class PanedQuadrant(Gtk.Bin):
     """ Linked Panes is a set of three paned widgets, one top level one
         and two nested ones that keep in sync with eachother.
 
@@ -14,31 +34,30 @@ class LinkedPanes(Gtk.Bin):
             br (Gtk.Widget): Bottom right widget
     """
     def __init__(self, tl, tr, bl, br):
-        super(LinkedPanes, self).__init__()
+        super(PanedQuadrant, self).__init__()
 
-        if 'linked-pane':
-            link1 = LinkedPane(
-                Gtk.Orientation.HORIZONTAL,
-                tl, tr,
-                (160, 90)
-            )
-            link2 = LinkedPane(
-                Gtk.Orientation.HORIZONTAL,
-                bl, br,
-                (160, 90)
-            )
-            link1.bind_resize(link2)
-            link2.bind_resize(link1)
+        # linked-pane
+        link1 = LinkedPane(
+            Gtk.Orientation.HORIZONTAL,
+            tl, tr,
+            (160, 90)
+        )
+        link2 = LinkedPane(
+            Gtk.Orientation.HORIZONTAL,
+            bl, br,
+            (160, 90)
+        )
+        link1.bind_resize(link2)
+        link2.bind_resize(link1)
 
-        if 'primary-pane':
-            self.add(
-                Pane(
-                    Gtk.Orientation.VERTICAL,
-                    link1, link2,
-                    (160, 90),
-                    Gtk.ShadowType.NONE
-                )
-            )
+        # primary-pane
+        self.add(Pane(
+            Gtk.Orientation.VERTICAL,
+            link1, link2,
+            (160, 90),
+            Gtk.ShadowType.NONE
+        ))
+
 
 class Pane(Gtk.Paned):
     """ Gtk.Paned wrapper for easier building.
@@ -66,6 +85,7 @@ class Pane(Gtk.Paned):
         self.pack2(self.frame2, resize=True, shrink=False)
         self.frame2.add(child2)
 
+
 class LinkedPane(Pane):
     """ Pane wrapper with methods for position synchronizing.
 
@@ -74,6 +94,7 @@ class LinkedPane(Pane):
     """
 
     _lp_user_activated = False
+    linked = None
 
     def __init__(self, *args):
         super(LinkedPane, self).__init__(*args)
@@ -81,7 +102,7 @@ class LinkedPane(Pane):
         self.connect('button-press-event', self.on_button_press)
         self.connect('button-release-event', self.on_button_release)
 
-    def on_position(self, _, gparamspec):
+    def on_position(self, *_):
         """ `notify::position` handler
             Updates sibling pane with new position.
         """
---
+++ setup.py
@@ -0,0 +1,31 @@
+"""
+    Gtk.PanedQuadrent
+"""
+
+import setuptools
+setuptools.setup(
+    name='gtk-panedquadrent',
+    version='1.0.0',
+    description='Synchronized Nested Gtk.Paned Widgets',
+    url='https://github.com/thorsummoner/Gtk.PanedQuandrent',
+    author='Dylan Scott Grafmyre',
+    author_email='thorsummoner@live.com',
+    license='LGPLv2+',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Environment :: X11 Applications :: GTK',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='gtk gui paned linked quadrant development',
+    py_modules=["gtk_panedquadrent"],
+)
